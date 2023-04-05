2a03a9438a5a8acd1bf317158640abe7b74a1598
Robert Gripshover <robertgrips@gmail.com>
2023-04-05 09:44:48 -0400

    m

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
 
 const authRouter = require('./auth/auth-router.js')
 const usersRouter = require('./users/users-router.js')
@@ -10,6 +11,20 @@ const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
+server.use(session({
+  name: 'monkey',
+  secret: 'keep it secret',
+  cookie: {
+    maxAge: 1000 * 60 *60, //<< this is in milli seconds
+    //so it needs to be a pretty big number
+    secure: false, //<<< if this was true it would only work on https
+    httpOnly: false, //<<< the js on the page CAN read the cookie
+  },
+  resave: false, //<<< don't worry about this right now just set it to false
+  saveUninitialized: false, // <<< this means we can't be setting cookies
+  // on any client that makes requests. We can only set a cookie on 
+  //successful login
+}))
 
 server.use('/api/auth', authRouter)
 
